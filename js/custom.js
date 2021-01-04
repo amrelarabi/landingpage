38aa6641804bd624c12ad39f45e40f38d017f091
amrelarabi <amr.elarabi.egy@gmail.com>
2021-01-04 02:35:50 +0200

    Landing page working prototype

---
+++ js/custom.js
@@ -0,0 +1,271 @@
+/* =================================
+   LOADER                     
+=================================== */
+// makes sure the whole site is loaded
+jQuery(window).load(function() {
+        // will first fade out the loading animation
+	jQuery(".status").fadeOut();
+        // will fade out the whole DIV that covers the website.
+	jQuery(".preloader").delay(1000).fadeOut("slow");
+})
+
+/* =================================
+===  SMOOTHSCOLLING       ====
+=================================== */
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+/* =================================
+===  PARALEXED EFFECT        ====
+=================================== */
+var $windows = $(window);
+
+$('section[data-type="background"]').each(function(){
+	var $bgobj = $(this); // assigning the object
+
+	$(window).scroll(function() {
+
+		var yPos = -($windows.scrollTop() / $bgobj.data('speed')); 
+
+		// Put together our final background position
+		var coords = '50% '+ yPos + 'px';
+
+		$($bgobj).css({'background-position': coords});
+	}); 
+});   
+	
+
+/* NAVIGATION VISIBLE ON SCROLL */
+
+$(document).ready(function () {
+    mainNav();
+});
+
+$(window).scroll(function () {
+    mainNav();
+});
+
+if (matchMedia('(min-width: 992px), (max-width: 767px)').matches) {
+  function mainNav() {
+        var top = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
+        if (top > 20) {
+			$('#top_nav').addClass("scroll_nav");
+		}
+
+        else { 
+			$('#top_nav').removeClass("scroll_nav");
+		}
+    }
+}
+
+if (matchMedia('(min-width: 768px) and (max-width: 991px)').matches) {
+  function mainNav() {
+        var top = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
+        if (top > 40) $('.sticky-navigation').stop().animate({"top": '0'});
+
+        else $('.sticky-navigation').stop().animate({"top": '-120'});
+    }
+}
+
+/* =================================
+===  WAYPOINT progress bar               ====
+=================================== */
+
+var waypoint = new Waypoint({
+  element: document.getElementById('progress'),
+  handler: function(direction) {
+	 
+	$('.progress-bar').each(function(){
+		W = $(this).data("progress") + "%";
+		$(this).animate({width:W} , "very slow");
+	});
+
+	  
+	this.destroy()
+  },
+   offset:"50%"
+})
+
+var waypoint = new Waypoint({
+  element: document.getElementById('numbers'),
+  handler: function(direction) {
+	  $('.number').each(function () {
+			$(this).prop('Counter',0).animate({
+				Counter: $(this).text()
+				}, {
+					duration: 4000,
+					easing: 'swing',
+					step: function (now) {
+						$(this).text(Math.ceil(now));
+					}
+				});
+			});
+	  this.destroy()
+  },
+   offset:"100%"
+})
+
+
+
+/* =================================
+===  OWL CROUSEL               ====
+=================================== */
+
+$(document).ready(function() {
+	 $("#latest_news").owlCarousel({
+   	      autoPlay: 3000, //Set AutoPlay to 3 seconds
+		  items : 3,
+		  itemsDesktop : [1199,3],
+		  itemsDesktopSmall : [979,3]
+		});
+});
+
+
+/* =================================
+===  DOWNLOAD BUTTON CLICK SCROLL ==
+=================================== */
+jQuery(function( $ ){
+	$('#download-button').localScroll({
+		duration:1000
+	});
+});
+
+
+/* =================================
+===  FULL SCREEN HEADER         ====
+=================================== */
+function alturaMaxima() {
+  var altura = $(window).height();
+  $(".full-screen").css('min-height',altura); 
+  
+}
+
+$(document).ready(function() {
+  alturaMaxima();
+  $(window).bind('resize', alturaMaxima);
+});
+
+/* =================================
+===  WOW ANIMATION             ====
+=================================== */
+wow = new WOW(
+  {
+  boxClass:     'wow',      // default
+  animateClass: 'animated', // default
+  offset:       0,          // default
+  mobile:       true,       // default
+  live:         true        // default
+}
+)
+wow.init();
+
+
+
+/* =================================
+===  STELLAR                    ====
+=================================== */
+$(window).stellar({ 
+horizontalScrolling: false 
+});
+$(".parallexed").stellar();
+
+/* =================================
+===  SUBSCRIPTION FORM          ====
+=================================== */
+$("#subscribe").submit(function (e) {
+    e.preventDefault();
+    var email = $("#subscriber-email").val();
+    var dataString = 'email=' + email;
+
+    function isValidEmail(emailAddress) {
+        var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
+        return pattern.test(emailAddress);
+    };
+
+    if (isValidEmail(email)) {
+        $.ajax({
+            type: "POST",
+            url: "subscribe/subscribe.php",
+            data: dataString,
+            success: function () {
+                $('.subscription-success').fadeIn(1000);
+                $('.subscription-error').fadeOut(500);
+                $('.hide-after').fadeOut(500);
+            }
+        });
+    } else {
+        $('.subscription-error').fadeIn(1000);
+    }
+
+    return false;
+});
+
+
+/* =================================
+===  CONTACT FORM          ====
+=================================== */
+$("#contact").submit(function (e) {
+    e.preventDefault();
+    var name = $("#name").val();
+    var email = $("#email").val();
+    var subject = $("#subject").val();
+    var message = $("#message").val();
+    var dataString = 'name=' + name + '&email=' + email + '&subject=' + subject + '&message=' + message;
+
+    function isValidEmail(emailAddress) {
+        var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
+        return pattern.test(emailAddress);
+    };
+
+    if (isValidEmail(email) && (message.length > 1) && (name.length > 1)) {
+        $.ajax({
+            type: "POST",
+            url: "sendmail.php",
+            data: dataString,
+            success: function () {
+                $('.success').fadeIn(1000);
+                $('.error').fadeOut(500);
+            }
+        });
+    } else {
+        $('.error').fadeIn(1000);
+        $('.success').fadeOut(500);
+    }
+
+    return false;
+});
+
+
+
+/* =================================
+===  EXPAND COLLAPSE            ====
+=================================== */
+$('.expand-form').simpleexpand({
+    'defaultTarget': '.expanded-contact-form'
+});
+
+
+/* =================================
+===  IE10 ON WINDOWS 8 FIX      ====
+=================================== */
+if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+  var msViewportStyle = document.createElement('style')
+  msViewportStyle.appendChild(
+    document.createTextNode(
+      '@-ms-viewport{width:auto!important}'
+    )
+  )
+  document.querySelector('head').appendChild(msViewportStyle)
+}
